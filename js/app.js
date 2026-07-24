f50566d3face34251f127fb2bd8562c8fe4fb58f
iku190t <iku190t@users.noreply.github.com>
2026-07-24 23:03:42 +0900

    Resize voice controls and soften point actions

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ import {
   LEVELING_TOLERANCE_PRESETS,
   sumObservationDistanceMeters,
   toNumber
-} from "./calculation.js?v=69";
+} from "./calculation.js?v=70";
 import {
   chooseLevelReading,
   createVoiceController,
@@ -14,13 +14,13 @@ import {
   normalizeSpokenNumber,
   prepareSpeechSynthesis,
   speakBack
-} from "./voice.js?v=69";
-import { clearProject, loadProject, saveProject } from "./storage.js?v=69";
-import { exportSheetCsv } from "./export.js?v=69";
+} from "./voice.js?v=70";
+import { clearProject, loadProject, saveProject } from "./storage.js?v=70";
+import { exportSheetCsv } from "./export.js?v=70";
 import {
   isValidStaffReading,
   reversePointNamesWithinUsedRows
-} from "./rules.js?v=69";
+} from "./rules.js?v=70";
 import {
   choosePointName,
   getRankedPointNameCandidates,
@@ -28,7 +28,7 @@ import {
   normalizePointName,
   pointNameToSpeech,
   recordPointNameUsage
-} from "./point-names.js?v=69";
+} from "./point-names.js?v=70";
 
 const DEFAULT_ROW_COUNT = 200;
 const POINT_SUGGESTION_LIMIT = 10;
--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,15 @@
-const CACHE_NAME = "suijun-voice-book-v69";
+const CACHE_NAME = "suijun-voice-book-v70";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./css/style.css?v=69",
-  "./js/app.js?v=69",
-  "./js/calculation.js?v=69",
-  "./js/voice.js?v=69",
-  "./js/storage.js?v=69",
-  "./js/export.js?v=69",
-  "./js/rules.js?v=69",
-  "./js/point-names.js?v=69",
+  "./css/style.css?v=70",
+  "./js/app.js?v=70",
+  "./js/calculation.js?v=70",
+  "./js/voice.js?v=70",
+  "./js/storage.js?v=70",
+  "./js/export.js?v=70",
+  "./js/rules.js?v=70",
+  "./js/point-names.js?v=70",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"
