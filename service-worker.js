341ebafb5e5f1a142fcd26b3768f7087ce754d6d
iku190t <iku190t@users.noreply.github.com>
2026-08-16 23:42:12 +0900

    Remove remaining header underline

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "suijun-voice-book-v194";
+const CACHE_NAME = "suijun-voice-book-v195";
 const APP_ASSETS = [
   "./",
   "./index.html",
-  "./css/style.css?v=194",
-  "./js/app.js?v=194",
+  "./css/style.css?v=195",
+  "./js/app.js?v=195",
   "./js/calculation.js?v=190",
   "./js/voice.js?v=190",
   "./js/storage.js?v=190",
